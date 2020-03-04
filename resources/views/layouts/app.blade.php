62f7baa15f613dd8029f6a9d585038102cfe5b20
uni51 <uni51@users.noreply.github.com>
2020-03-04 14:37:29 +0900

    4-4：仮トップページのHTML作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img class='navbar-logo' src="{{ asset('images/logo.png') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -73,9 +74,12 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="main">
             @yield('content')
         </main>
+        <footer class='footer p20'>
+            <small class='copyright'>Laravel Book Reviews 2019 copyright</small>
+        </footer>
     </div>
 </body>
 </html>
