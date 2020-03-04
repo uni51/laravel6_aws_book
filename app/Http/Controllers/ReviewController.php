3faa5dcc74142f7e46a3f17e8630e2439ea17def
uni51 <uni51@users.noreply.github.com>
2020-03-04 15:07:27 +0900

    5-3：レビュー登録機能の開発

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -14,5 +15,15 @@ class ReviewController extends Controller
     public function create()
     {
         return view('review');
+    }
+
+    public function store(Request $request)
+    {
+        $post = $request->all();
+        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+
+        Review::insert($data);
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ Route::get('/' , 'ReviewController@index')->name('index');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-
     Route::get('/review', 'ReviewController@create')->name('create');
-
+    Route::post('/review/store', 'ReviewController@store')->name('store');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
