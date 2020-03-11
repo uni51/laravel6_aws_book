870a0ef7fd4f355e1d29a2b7fa0fe6f602b2a273
uni51 <uni51@users.noreply.github.com>
2020-03-11 15:31:44 +0900

    5-5：バリデーションの追加

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -21,6 +21,12 @@ class ReviewController extends Controller
     {
         $post = $request->all();
 
+        $validatedData = $request->validate([
+            'title' => 'required | max:255',
+            'body' => 'required',
+            'image' => 'mimes:jpeg,png,jpg,gif,svg | max:2048',
+        ]);
+
         if ($request->hasFile('image')) {
             // storage/app/public/images配下にファイルをアップロード
             $request->file('image')->store('/public/images');
