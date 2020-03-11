cd2054fb8d67676b491910f1976892c6caba5310
uni51 <uni51@users.noreply.github.com>
2020-03-11 16:05:31 +0900

    6-1：レビューデータの取得と一覧表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,17 +6,27 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card mb50">
-                <div class="card-body">
-                    <div class='image-wrapper'><img class='book-image' src="{{ asset('images/dummy.png') }}"></div>
-                    <h3 class='h3 book-title'>タイトル</h3>
-                    <p class='description'>
-                        レビュー本文
-                    </p>
-                    <a href=""class='btn btn-secondary detail-btn'>詳細を読む</a>
+
+        @foreach($reviews as $review)
+
+            <div class="col-md-4">
+                <div class="card mb50">
+                    <div class="card-body">
+                        <div class='image-wrapper'><img class='book-image' src="{{ asset('images/dummy.png') }}"></div>
+
+                        <h3 class='h3 book-title'>{{ $review->title }}</h3>
+
+                        <p class='description'>
+                            {{ $review->body }}
+                        </p>
+                        <a href=""class='btn btn-secondary detail-btn'>詳細を読む</a>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        @endforeach
     </div>
+
+    {{ $reviews->links() }}
+    
 @endsection
