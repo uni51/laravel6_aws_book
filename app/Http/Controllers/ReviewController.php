775f3f8efebc72aca0b59ec2a31023c2e5fadfc5
uni51 <uni51@users.noreply.github.com>
2020-03-11 11:35:11 +0900

    5-4：画像のアップロード処理

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -20,7 +20,23 @@ class ReviewController extends Controller
     public function store(Request $request)
     {
         $post = $request->all();
-        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+
+        if ($request->hasFile('image')) {
+            // storage/app/public/images配下にファイルをアップロード
+            $request->file('image')->store('/public/images');
+            $data = [
+                'user_id' => \Auth::id(),
+                'title' => $post['title'],
+                'body' => $post['body'],
+                'image' => $request->file('image')->hashName()
+            ];
+        } else {
+            $data = [
+                'user_id' => \Auth::id(),
+                'title' => $post['title'],
+                'body' => $post['body']
+            ];
+        }
 
         Review::insert($data);
 
